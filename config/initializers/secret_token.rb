d8376bbaaa32fe072ced16ecbcc62fa118b4331a
Michael Nugent <mike@illuminatus.org>
2013-02-09 18:17:30 -0800

    initial commit, devise model gen run as User

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GenericTemplate::Application.config.secret_token = '009d318d5690a73269bcd69392b6ea5a2730b1bbc54d788c683d6b95dcfba7c87eaf4e6d8d19eb92ca0678e12778323264cf074e6ab2f5212e0607ec1af2c2d7'
